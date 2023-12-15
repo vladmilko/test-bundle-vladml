5ff0ec8ae04efa01311a0bc5c5da7bdf883c84ee
vladmilko <vladmilko27@gmail.com>
2023-12-15 09:41:07 +0300

    Добавит плейсхолдер

--- src/TextInput/TextInput.tsx
+++ src/TextInput/TextInput.tsx
@@ -2,16 +2,17 @@ import { useController, useForm } from 'react-hook-form';
 
 export interface TextInputProps {
   name: string;
+  placeholder: string;
   className?: string;
 }
 
-export const TextInput = ({ name, className }: TextInputProps) => {
+export const TextInput = ({ name, className, placeholder }: TextInputProps) => {
   const { control } = useForm();
   const { field } = useController({ name, control });
 
   return (
     <div className={className}>
-      <input {...field} type="text" />
+      <input {...field} type="text" placeholder={placeholder} />
     </div>
   );
 };
